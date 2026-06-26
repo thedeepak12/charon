1a6019330222ad333b148da234e3111af4a0c67b
Deepak <206972401+thedeepak12@users.noreply.github.com>
2026-06-26 08:42:37 +0530

    Implement token bucket allow logic

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/thedeepak12/charon/internal/ratelimiter"
 )
 
 func main() {
 	config := ratelimiter.DefaultConfig()
+	config.Capacity = 20
+	config.RefillRate = 1
+	config.Interval = 1 * time.Second
 
 	bucket, err := ratelimiter.NewTokenBucket(config)
 	if err != nil {
@@ -16,4 +20,10 @@ func main() {
 	}
 
 	fmt.Printf("Bucket initialized:\n%v\n", bucket)
+
+	for i := 0; i < 50; i++ {
+		allowed := bucket.Allow()
+		fmt.Printf("Request %d: Allowed = %v\n Tokens = %d\n", i+1, allowed, bucket.Tokens())
+		time.Sleep(500 * time.Millisecond)
+	}
 }
--- internal/ratelimiter/token_bucket.go
+++ internal/ratelimiter/token_bucket.go
@@ -29,3 +29,39 @@ func (tb *TokenBucket) String() string {
 	return fmt.Sprintf("TokenBucket{Capacity: %d, Tokens: %d, RefillRate: %d, Interval: %v}",
 		tb.config.Capacity, tb.tokens, tb.config.RefillRate, tb.config.Interval)
 }
+
+func (tb *TokenBucket) Allow() bool {
+	tb.mu.Lock()
+	defer tb.mu.Unlock()
+
+	tokensToAdd := tb.calculateTokensToAdd()
+
+	if tokensToAdd > 0 {
+		tb.lastRefill = time.Now()
+	}
+
+	tb.tokens += tokensToAdd
+	if tb.tokens > tb.config.Capacity {
+		tb.tokens = tb.config.Capacity
+	}
+
+	if tb.tokens > 0 {
+		tb.tokens--
+		return true
+	}
+
+	return false
+}
+
+func (tb *TokenBucket) calculateTokensToAdd() int64 {
+	elapsed := time.Since(tb.lastRefill)
+	intervalsPassed := elapsed / tb.config.Interval
+	tokensToAdd := int64(intervalsPassed) * tb.config.RefillRate
+	return tokensToAdd
+}
+
+func (tb *TokenBucket) Tokens() int64 {
+	tb.mu.Lock()
+	defer tb.mu.Unlock()
+	return tb.tokens
+}
